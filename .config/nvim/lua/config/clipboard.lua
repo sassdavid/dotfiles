7cb2555034370e91403cfa4c3963fe98adfd5134
sassdavid <david.sass14@gmail.com>
2025-03-27 14:07:27 +0100

    clipboard settings

--- .config/nvim/lua/config/clipboard.lua
+++ .config/nvim/lua/config/clipboard.lua
@@ -1,12 +1,12 @@
 vim.g.clipboard = {
     name = "WslClipboard",
     copy = {
-        ["+"] = "clip.exe",
-        ["*"] = "clip.exe",
+        ["+"] = "/c/Windows/System32",
+        ["*"] = "/c/Windows/System32",
     },
     paste = {
-        ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace("`r", ""))',
-        ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace("`r", ""))',
+        ["+"] = '/c/Program Files/PowerShell/7/pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace("`r", ""))',
+        ["*"] = '/c/Program Files/PowerShell/7/pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace("`r", ""))',
     },
     cache_enabled = 0,
 }
